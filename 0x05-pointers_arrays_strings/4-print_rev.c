54fee462b182281da894838c528285de2a84dd99
Rayanomer24 <rayanomer114@gmail.com>
2022-12-19 16:26:15 -0800

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <string.h>
 /**
  * print_rev - function that prints a string, in reverse
  * @s: the string to be printed in rev
  */
 void print_rev(char *s)
 {
-	int n, i;
-	s = "Holberton!";
-	n = strlen(s);
-	for (i = n-1; i >= 0; i--)
-	{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i-1; i >= 0; i--)
 		_putchar(s[i]);
-	}
+
+	_putchar('\n');
 }
