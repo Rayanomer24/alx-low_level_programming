e82b67598a7a151191d833f27e401207a4ae9e5f
Rayanomer24 <rayanomer114@gmail.com>
2023-01-02 15:22:28 -0800

    0

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strstr - prints the consecutive caracters of s1 that are in s2. 
  * @haystack: source string 
@@ -7,18 +8,15 @@
  */
 char *_strstr(char *haystack, char *needle)
 {		
-	
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
 		char *p = needle;
-
 		while (*l == *p && *p != '\0')
 		{
 			l++;
 			p++;
 		}
-		
 		if (*p == '\0')
 			return (haystack);
 	}
